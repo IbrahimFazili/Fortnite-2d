6fab5c84710562cc51bd213d1e49db7e5413dd78
Ibrahim Fazili <ibrahim.fazili@mail.utoronto.ca>
2021-03-04 22:49:28 +0000

    added dynamic and static object classes

--- ftd_starter/static_content/controller.js
+++ ftd_starter/static_content/controller.js
@@ -1,80 +1,101 @@
-var stage=null;
+var stage = null;
 var view = null;
-var interval=null;
-var credentials={ "username": "", "password":"" };
-function setupGame(){
-	stage=new Stage(document.getElementById('stage'));
+var interval = null;
+var credentials = { "username": "", "password": "" };
+var lastKey = [];
+function setupGame() {
+	stage = new Stage(document.getElementById('stage'));
 
 	// https://javascript.info/keyboard-events
-	document.addEventListener('keydown', moveByKey);
+	document.addEventListener('keydown', () => moveByKey(event, false));
+	document.addEventListener('keyup', () => moveByKey(event, true))
 }
-function startGame(){
-	interval=setInterval(function(){ stage.step(); stage.draw(); },16.66);
+function startGame() {
+	interval = setInterval(function () { stage.step(); stage.draw(); }, 16.66);
 }
-function pauseGame(){
+function pauseGame() {
 	clearInterval(interval);
-	interval=null;
+	interval = null;
 }
-function moveByKey(event){
+function moveByKey(event, released) {
 	var key = event.key;
-	var moveMap = { 
-		'a': new Pair(-5,0),
-		's': new Pair(0,5),
-		'd': new Pair(5,0),
-		'w': new Pair(0,-5)
+	var moveMap = {
+		'a': new Pair(-3, 0),
+		's': new Pair(0, 3),
+		'd': new Pair(3, 0),
+		'w': new Pair(0, -3)
 	};
-	if(key in moveMap){
-		stage.player.velocity=moveMap[key];
+	if (released) {
+		keyIndex = lastKey.findIndex((value) => value === key);
+		if (keyIndex !== -1) {
+			lastKey.splice(keyIndex, 1);
+			const p = stage.player;
+			const oldV = moveMap[key];
+			const negated = new Pair(-oldV.x, -oldV.y);
+			p.velocity = p.velocity.add(negated)
+			console.log(`${key} released | new V: ${p.velocity} | old: ${oldV}`);
+		}
+
+		return;
+	}
+
+	if (key in moveMap) {
+		keyIndex = lastKey.findIndex((value) => value === key);
+		if (keyIndex === -1) {
+			lastKey.push(key);
+			const p = stage.player;
+			p.velocity = p.velocity.add(moveMap[key])
+		}
 	}
 }
 
-function login(){
-	credentials =  { 
-		"username": $("#username").val(), 
-		"password": $("#password").val() 
+function login() {
+	credentials = {
+		"username": $("#username").val(),
+		"password": $("#password").val()
 	};
 
-        $.ajax({
-                method: "POST",
-                url: "/api/auth/login",
-                data: JSON.stringify({}),
+	$.ajax({
+		method: "POST",
+		url: "/api/auth/login",
+		data: JSON.stringify({}),
 		headers: { "Authorization": "Basic " + btoa(credentials.username + ":" + credentials.password) },
-                processData:false,
-                contentType: "application/json; charset=utf-8",
-                dataType:"json"
-        }).done(function(data, text_status, jqXHR){
-                console.log(jqXHR.status+" "+text_status+JSON.stringify(data));
+		processData: false,
+		contentType: "application/json; charset=utf-8",
+		dataType: "json"
+	}).done(function (data, text_status, jqXHR) {
+		console.log(jqXHR.status + " " + text_status + JSON.stringify(data));
 
-        	$("#ui_login").hide();
-        	$("#ui_play").show();
+		$("#ui_login").hide();
+		$("#ui_play").show();
 
 		setupGame();
 		startGame();
 
-        }).fail(function(err){
-                console.log("fail "+err.status+" "+JSON.stringify(err.responseJSON));
-        });
+	}).fail(function (err) {
+		console.log("fail " + err.status + " " + JSON.stringify(err.responseJSON));
+	});
 }
 
 // Using the /api/auth/test route, must send authorization header
-function test(){
-        $.ajax({
-                method: "GET",
-                url: "/api/auth/test",
-                data: {},
+function test() {
+	$.ajax({
+		method: "GET",
+		url: "/api/auth/test",
+		data: {},
 		headers: { "Authorization": "Basic " + btoa(credentials.username + ":" + credentials.password) },
-                dataType:"json"
-        }).done(function(data, text_status, jqXHR){
-                console.log(jqXHR.status+" "+text_status+JSON.stringify(data));
-        }).fail(function(err){
-                console.log("fail "+err.status+" "+JSON.stringify(err.responseJSON));
-        });
+		dataType: "json"
+	}).done(function (data, text_status, jqXHR) {
+		console.log(jqXHR.status + " " + text_status + JSON.stringify(data));
+	}).fail(function (err) {
+		console.log("fail " + err.status + " " + JSON.stringify(err.responseJSON));
+	});
 }
 
-$(function(){
-        // Setup all events here and display the appropriate UI
-        $("#loginSubmit").on('click',function(){ login(); });
-        $("#ui_login").show();
-        $("#ui_play").hide();
+$(function () {
+	// Setup all events here and display the appropriate UI
+	$("#loginSubmit").on('click', function () { login(); });
+	$("#ui_login").show();
+	$("#ui_play").hide();
 });
 
--- ftd_starter/static_content/model.js
+++ ftd_starter/static_content/model.js
@@ -1,50 +1,55 @@
-function randint(n){ return Math.round(Math.random()*n); }
-function rand(n){ return Math.random()*n; }
+function randint(n) { return Math.round(Math.random() * n); }
+function rand(n) { return Math.random() * n; }
 
 class Stage {
-	constructor(canvas){
+	constructor(canvas) {
 		this.canvas = canvas;
-	
-		this.actors=[]; // all actors on this stage (monsters, player, boxes, ...)
-		this.player=null; // a special actor, the player
-	
+
+		this.actors = []; // all actors on this stage (monsters, player, boxes, ...)
+		this.player = null; // a special actor, the player
+
+		// logical width and height of the world
+		this.worldwidth = 5000
+		this.worldheight = 5000
+
 		// the logical width and height of the stage
-		this.width=window.innerWidth;
-		this.height=window.innerHeight;
+		this.width = window.innerWidth;
+		this.height = window.innerHeight;
 
 		// Add the player to the center of the stage
-		var velocity = new Pair(0,0);
+		var velocity = new Pair(0, 0);
 		var radius = 20;
-		var colour= 'rgba(0,0,0,1)';
-		var position = new Pair(Math.floor(this.width/2), Math.floor(this.height/2));
-		this.addPlayer(new Player(this, position, velocity, colour, radius));
+		var health = 100.0;
+		var colour = 'rgba(0,0,0,1)';
+		var position = new Pair(Math.floor(this.width / 2), Math.floor(this.height / 2));
+		this.addPlayer(new Player(this, position, health, colour, radius));
 	}
 
-	addPlayer(player){
+	addPlayer(player) {
 		this.addActor(player);
-		this.player=player;
+		this.player = player;
 	}
 
-	removePlayer(){
+	removePlayer() {
 		this.removeActor(this.player);
-		this.player=null;
+		this.player = null;
 	}
 
-	addActor(actor){
+	addActor(actor) {
 		this.actors.push(actor);
 	}
 
-	removeActor(actor){
-		var index=this.actors.indexOf(actor);
-		if(index!=-1){
-			this.actors.splice(index,1);
+	removeActor(actor) {
+		var index = this.actors.indexOf(actor);
+		if (index != -1) {
+			this.actors.splice(index, 1);
 		}
 	}
 
 	// Take one step in the animation of the game.  Do this by asking each of the actors to take a single step. 
 	// NOTE: Careful if an actor died, this may break!
-	step(){
-		for(var i=0;i<this.actors.length;i++){
+	step() {
+		for (var i = 0; i < this.actors.length; i++) {
 			this.actors[i].step();
 		}
 	}
@@ -56,21 +61,51 @@ class Stage {
 		this.height = ctx.canvas.height;
 	}
 
-	draw(){
+	draw() {
 		var context = this.canvas.getContext('2d');
+		
 		this.setGameWindowSize(context);
-		context.clearRect(0, 0, this.width, this.height);
-		context.fillStyle = "rgba(1, 143, 6, 1)";
-		context.fillRect(0, 0, this.width, this.height);
-		for(var i=0;i<this.actors.length;i++){
+
+		// context.clearRect(0, 0, this.width, this.height);
+		// context.fillStyle = "rgba(1, 143, 6, 1)";
+		// context.fillRect(0, 0, this.width, this.height);
+		// context.setTransform(1, 0, 0, 1, 0, 0)
+
+		let cols = this.width / 10 
+		let rows = this.height / 10
+		let squareSize = 50
+
+		this.drawCheckeredBoard(context, squareSize, rows, cols)
+
+		for (var i = 0; i < this.actors.length; i++) {
 			this.actors[i].draw(context);
 		}
+
+	}
+
+	clamp(value, min, max){
+		if(value < min) return min;
+		else if(value > max) return max;
+		return value;
+	}
+
+	drawCheckeredBoard(ctx, squareSize, rows, cols) {
+		let whiteSquareColor = "#ffe6cc"
+		let blackSquareColor = "#cc6600"
+	
+		for (let j = 0; j < rows; j++)
+			for (let i = 0; i < cols; i++) {
+				if ((i % 2 == 0 && j % 2 == 0) || (i % 2 != 0 && j % 2 != 0)) 
+					ctx.fillStyle = whiteSquareColor
+				else ctx.fillStyle = blackSquareColor
+				ctx.fillRect(i * squareSize, j * squareSize, squareSize, squareSize)
+			}
 	}
 
 	// return the first actor at coordinates (x,y) return null if there is no such actor
-	getActor(x, y){
-		for(var i=0;i<this.actors.length;i++){
-			if(this.actors[i].x==x && this.actors[i].y==y){
+	getActor(x, y) {
+		for (var i = 0; i < this.actors.length; i++) {
+			if (this.actors[i].x == x && this.actors[i].y == y) {
 				return this.actors[i];
 			}
 		}
@@ -79,86 +114,141 @@ class Stage {
 } // End Class Stage
 
 class Pair {
-	constructor(x,y){
-		this.x=x; this.y=y;
+	constructor(x, y) {
+		this.x = x; this.y = y;
 	}
 
-	toString(){
-		return "("+this.x+","+this.y+")";
+	toString() {
+		return "(" + this.x + "," + this.y + ")";
 	}
 
-	normalize(){
-		var magnitude=Math.sqrt(this.x*this.x+this.y*this.y);
-		this.x=this.x/magnitude;
-		this.y=this.y/magnitude;
+	normalize() {
+		var magnitude = Math.sqrt(this.x * this.x + this.y * this.y);
+		this.x = this.x / magnitude;
+		this.y = this.y / magnitude;
+	}
+
+	add(other) {
+		return new Pair(this.x + other.x, this.y + other.y);
 	}
 }
 
-class Ball {
-	constructor(stage, position, velocity, colour, radius){
-		this.stage = stage;
-		this.position=position;
-		this.intPosition(); // this.x, this.y are int version of this.position
+class GameObject {
 
-		this.velocity=velocity;
-		this.colour = colour;
-		this.radius = radius;
+	/**
+	 * @param position Pair
+	 * @param health float
+	 * @param color RGB string
+	 */
+	constructor(game, position, health, color = undefined) {
+		this.game = game;
+		this.position = position;
+		this.health = health;
+		this.color = color !== undefined ? color : `rgb(${randint(255)}, ${randint(255)}, ${randint(255)})`;
+	}
+
+	toString() {
+		return `Pos: ${this.position.toString()} | Health: ${this.health}`;
 	}
 	
-	headTo(position){
-		this.velocity.x=(position.x-this.position.x);
-		this.velocity.y=(position.y-this.position.y);
+	step() {
+		return;
+	}
+
+	intPosition() {
+		x = Math.round(this.position.x);
+		y = Math.round(this.position.y);
+		return new Pair(x, y);
+	}
+
+}
+
+class DynamicObjects extends GameObject {
+
+	/**
+	 *  DynamicObjects are those that move in the game
+	 */
+	constructor(game, position, health, color = undefined) {
+		super(game, position, health, color);
+		// initally, object is still
+		this.velocity = new Pair(0, 0);
+	}
+
+	toString() {
+		return `${super.toString()} | v ${this.velocity.toString()}`;
+	}
+
+	/**
+	 * Called on every game tick (every time the game state updates)
+	 */
+	step() {
+		this.position.x = this.position.x + this.velocity.x;
+		this.position.y = this.position.y + this.velocity.y;
+
+		// bounce off the walls
+		if (this.position.x < 0) {
+			this.position.x = 0;
+		}
+		if (this.position.x > this.game.width - 20) {
+			this.position.x = this.game.width - 20;
+		}
+		if (this.position.y < 0) {
+			this.position.y = 0;
+		}
+		if (this.position.y > this.game.height - 20) {
+			this.position.y = this.game.height - 20;
+		}
+	}
+
+	moveTo(position) {
+		this.velocity.x = (position.x - this.position.x);
+		this.velocity.y = (position.y - this.position.y);
 		this.velocity.normalize();
 	}
+}
 
-	toString(){
-		return this.position.toString() + " " + this.velocity.toString();
-	}
+class StaticObjects extends GameObject {
 
-	step(){
-		this.position.x=this.position.x+this.velocity.x;
-		this.position.y=this.position.y+this.velocity.y;
-			
-		// bounce off the walls
-		if(this.position.x<0){
-			this.position.x=0;
-			this.velocity.x=Math.abs(this.velocity.x);
-		}
-		if(this.position.x>this.stage.width){
-			this.position.x=this.stage.width;
-			this.velocity.x=-Math.abs(this.velocity.x);
-		}
-		if(this.position.y<0){
-			this.position.y=0;
-			this.velocity.y=Math.abs(this.velocity.y);
-		}
-		if(this.position.y>this.stage.height){
-			this.position.y=this.stage.height;
-			this.velocity.y=-Math.abs(this.velocity.y);
-		}
-		this.intPosition();
-	}
-	intPosition(){
-		this.x = Math.round(this.position.x);
-		this.y = Math.round(this.position.y);
-	}
-	draw(context){
-		context.fillStyle = this.colour;
-   		// context.fillRect(this.x, this.y, this.radius,this.radius);
-		context.beginPath(); 
-		context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false); 
-		context.fill();   
+	/**
+	 *  StaticObjects are those that don't move in the game
+	 */
+	constructor(game, position, health, color = undefined) {
+		super(game, position, health, color);
 	}
 }
 
-class Player extends Ball {
-	draw(context){
-		context.fillStyle = this.colour;
-   		context.fillRect(this.x, this.y, this.radius,this.radius);
-		/**
-		context.beginPath(); 
-		context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false); 
-		context.stroke();   
-		**/
+class Player extends DynamicObjects {
+
+	constructor(game, position, health, color, radius){
+		super(game, position, health, color)
+		this.radius = radius
+	}
+
+	draw(context) {
+		context.fillStyle = this.color;
+		context.fillRect(this.position.x, this.position.y, this.radius, this.radius);
+		// context.beginPath(); 
+		// context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false); 
+		// context.stroke();   
 	}
 }
+
+
+// dynamic objects
+
+	// player
+	// AI player
+	// bullets
+
+// static objects
+
+	// resources
+		// trees
+		// bricks
+		// metal
+
+	// walls
+
+	// weapons
+		// guns
+		// ammo
